37dae24f524efbb3aa6b9f2878cf68726be987d1
Singra <singratura@gmail.com>
2020-03-07 19:25:19 +0100

    Login

--- MitSeiv/src/app/pages/inicio-sesion/inicio-sesion.page.ts
+++ MitSeiv/src/app/pages/inicio-sesion/inicio-sesion.page.ts
@@ -1,11 +1,11 @@
-import { BaseDeDatos } from "src/app/interfaceServicios/baseDeDatos";
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { BaseDeDatos } from 'src/app/interfaceServicios/baseDeDatos';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-inicio-sesion",
-  templateUrl: "./inicio-sesion.page.html",
-  styleUrls: ["./inicio-sesion.page.scss"]
+  selector: 'app-inicio-sesion',
+  templateUrl: './inicio-sesion.page.html',
+  styleUrls: ['./inicio-sesion.page.scss']
 })
 export class InicioSesionPage implements OnInit {
   private correo: string;
@@ -17,13 +17,13 @@ export class InicioSesionPage implements OnInit {
   iniciarSesion() {
     this.base.iniciarSesion(this.correo, this.contrasena).then(r => {
       if (r) {
-        console.log("correcto");
+        console.log('correcto');
       } else {
-        console.log("fallo");
+        console.log('fallo');
       }
     });
   }
   registrar() {
-    this.router.navigate(["home"]);
+    this.router.navigate(['home']);
   }
 }
